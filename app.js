9d7b3244b034a4880e85ee2e39e322bcee73429c
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-06-20 02:00:47 +0430

    Added conditions

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const Admin = require('./modules/admin');
+const User = require('./modules/user');
 
 const app = express();
 
@@ -6,13 +8,36 @@ app.use(express.urlencoded({extended: true}));
 app.set('json spaces', 2);
 
 app.get('/api/access/:access_token/:user_token', (req, res) => {
-    const data = {
-        "access-token": req.params.access_token,
-        "user-token": req.params.user_token
-    };
-
     res.header("Access-Control-Allow-Origin", "*");
-    res.send(data);
+
+    Admin.findOne({access_token: req.params.access_token})
+        .then((admin_result) => {
+            User.findOne({token: req.params.user_token})
+                .then((user_result) => {
+                    const data = {
+                        error: 800,
+                        user: user_result
+                    };
+        
+                    res.send(data);
+                })
+                .catch((error) => {
+                    const data = {
+                        error: 820,
+                        message: "User authentication token is not valid"
+                    };
+        
+                    res.send(data);
+                });
+        })
+        .catch((error) => {
+            const data = {
+                error: 290,
+                message: "Admin access token is not valid"
+            };
+
+            res.send(data);
+        });    
 });
 
 app.listen(8000);
