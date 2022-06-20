76b405d056bed477e1fc2a716433c942252b9354
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-06-20 14:45:44 +0430

    Update module

--- modules/admin.js
+++ modules/admin.js
@@ -7,6 +7,18 @@ const adminModule = new Schema({
         type: String,
         required: true
     },
+    name: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
 }, {timestamps: true});
 
 const Admin = mongoose.model('admin', adminModule);
