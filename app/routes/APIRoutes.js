894b5b55370d06665b8ea3530f8bfde8b0c2727c
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-19 19:17:42 +0430

    Change post to get

--- app/routes/APIRoutes.js
+++ app/routes/APIRoutes.js
@@ -4,6 +4,6 @@ const controllers = require('../controllers/APIControllers');
 
 const routes = app.Router();
 
-routes.post('/access/:access_token/:user_token', controllers.AuthUser);
+routes.get('/access/:access_token/:user_token', controllers.AuthUser);
 
 module.exports = routes;
