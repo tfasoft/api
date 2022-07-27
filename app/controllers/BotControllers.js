0881478610fc11626d900f8f3abb4b5c99f7c4e2
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-27 21:35:22 +0430

    Add login bot

--- app/controllers/BotControllers.js
+++ app/controllers/BotControllers.js
@@ -1,9 +1,35 @@
+const randomstring = require("randomstring");
 const User = require('../models/user');
 
 const login = (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
 
-    res.send('Login');
+    const tid = req.body.tid;
+
+    const newToken = randomstring.generate({length: 25, charset: 'alphabetic'});
+
+    User.findOneAndUpdate({ tid: tid }, { token: newToken })
+        .then((result) => {
+            if (result === null) {
+                const data = {
+                    message: "You are not registered.\nPress register button to register."
+                }
+
+                res.status(401);
+                res.send(data);
+            } else {
+                const data = {
+                    token: newToken
+                }
+                
+                res.status(200);
+                res.send(data);
+            }
+        })
+        .catch((error) => {
+            res.status(500);
+            res.send(error);
+        });
 }
 
 const regsiter = (req, res) => {
@@ -14,7 +40,7 @@ const regsiter = (req, res) => {
 
 const info = (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
-    
+
     res.send('Login');
 }
 
