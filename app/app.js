ed737d9b99c931aef2b75f4c84f31c9463275208
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-28 10:38:28 +0430

    Now app use jso

--- app/app.js
+++ app/app.js
@@ -7,6 +7,8 @@ const BotRoutes = require('./routes/BotRoutes');
 const app = express();
 
 app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
 app.use(cors());
 
 app.set('json spaces', 2);
