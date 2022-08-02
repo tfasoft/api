1ea89f356523f19ef15b1ad7ae95034ed89c2bb1
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-02 11:39:40 +0430

    Change port and use ENV port

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const env = process.env;
 const mdb = `mongodb+srv://${env.MONGO_USERNAME}:${env.MONGO_PASSWORD}@${env.MONGO_DATABASE}.ji4jf.mongodb.net/${env.MONGO_COLLECTION}?retryWrites=true&w=majority`;
 mongoose.connect(mdb)
     .then((connection) => {
-        console.log('Connected');
-        app.listen(env.PORT || 8000);
+        const port = env.PORT;
+
+        console.log(`Connected. Running in ${port}`)
+        app.listen(port);
     })
     .catch((error) => console.log(error));
