8ea50ba93e926646d05def661a3a89b2e578e560
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-06 11:09:43 +0430

    Add app.use mobile routes

--- app/app.js
+++ app/app.js
@@ -2,17 +2,19 @@ const express = require('express');
 const cors = require('cors');
 
 const APIRoutes = require('./routes/APIRoutes');
+const MobileRoutes = require('./routes/MobileRoutes');
 const UserRoutes = require('./routes/UserRoutes');
 
 const app = express();
 
 app.use(express.urlencoded({extended: true}));
-app.use(express.json());
+app.use(express.json());    
 app.use(cors());
 
 app.set('json spaces', 2);
 
 app.use('/api/auth', APIRoutes);
+app.use('/api/mobile', MobileRoutes);
 app.use('/api/user', UserRoutes);
 
 module.exports = app;
