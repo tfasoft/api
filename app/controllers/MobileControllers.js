251d386dd5af7323ee65677876d99cd5fe8547df
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-06 11:52:58 +0430

    Done in fetch user

--- app/controllers/MobileControllers.js
+++ app/controllers/MobileControllers.js
@@ -1,12 +1,58 @@
-const login = (req, res) => {
+const User = require('../models/user');
+
+const auth = (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
 
-    res.status(200);
-    res.send({
-        message: "Hello",
-    });
+    const body = req.body;
+
+    User.findOne(body)
+        .then((result) => {
+            if (result === null) {
+                const data = {
+                    message: "User is not found",
+                }
+
+                res.status(401);
+                res.send(data);
+            }
+            else {
+                res.status(200);
+                res.send(result);
+            }
+        })
+        .catch((error) => {
+            res.status(500);
+            res.send(error);
+        });
+}
+
+const connect = (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+
+    const mcode = req.body.mcode;
+
+    User.findOne({ mcode })
+        .then((result) => {
+            if (result === null) {
+                const data = {
+                    message: "Sorry, you are not registered.",
+                }
+
+                res.status(401);
+                res.send(data);
+            }
+            else {
+                res.status(200);
+                res.send(result);
+            }
+        })
+        .catch((error) => {
+            res.status(500);
+            res.send(error);
+        });
 }
 
 module.exports = {
-    login,
+    auth,
+    connect,
 }
