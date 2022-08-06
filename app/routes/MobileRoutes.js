3dd627ff031c836a5a5df790e4d39732b2bdfeb7
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-06 11:53:19 +0430

    Change mobile auth endpoint and update endpoint

--- app/routes/MobileRoutes.js
+++ app/routes/MobileRoutes.js
@@ -4,6 +4,7 @@ const controllers = require("../controllers/MobileControllers");
 
 const router = express.Router();
 
-router.post('/login', controllers.login);
+router.post('/auth', controllers.auth);
+router.post('/connect', controllers.connect);
 
 module.exports = router;
