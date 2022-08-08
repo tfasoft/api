0d47e9c9a1e377e8179caee8bf17bc0dd3999faa
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-08 19:48:41 +0430

    New routes and stuff

--- app/app.js
+++ app/app.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const cors = require('cors');
 
-const AuthenticationRoutes = require('./routes/AuthenticationRoutes');
-const MobileRoutes = require('./routes/MobileRoutes');
-const UserRoutes = require('./routes/UserRoutes');
+const AuthenticationRoutes = require('./routes/authentication_routes');
+const BlogRoutes = require('./routes/blog_routes');
+const DashboardRoutes = require('./routes/dashboard_routes');
+const DemoRoutes = require('./routes/demo_routes');
+const MobileRoutes = require('./routes/mobile_routes');
+const UserRoutes = require('./routes/user_routes');
 
 const app = express();
 
+app.set('json spaces', 2);
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());    
 app.use(cors());
 
-app.set('json spaces', 2);
-
 app.use('/api/auth', AuthenticationRoutes);
+app.use('/api/blog', BlogRoutes);
+app.use('/api/dashboard', DashboardRoutes);
+app.use('/api/demo', DemoRoutes);
 app.use('/api/mobile', MobileRoutes);
 app.use('/api/user', UserRoutes);
 
