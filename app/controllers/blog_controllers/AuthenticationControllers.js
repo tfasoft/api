f1c40fea4d672d2b9c13c2765dc01d55f714a9a6
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-08 19:50:27 +0430

    Add blog controllers

---
+++ app/controllers/blog_controllers/AuthenticationControllers.js
@@ -0,0 +1,56 @@
+const Author = require("../../models/author");
+
+const login = (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+
+    const userInputData = req.body;
+
+    Author.findOne(userInputData)
+        .then((author) => {
+            if (author === null) {
+                const data = {
+                    message: "Sorry author is not here!",
+                }
+
+                res.status(401);
+                res.send(data);
+            } else {
+                const data = {
+                    author,
+                }
+
+                res.status(200);
+                res.send(data);
+            }
+        })
+        .catch((error) => {
+            res.status(500);
+            res.send(error);
+        })
+}
+
+const register = (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+
+    const userInputData = req.body;
+
+    const author = new Author(userInputData);
+    author.save()
+        .then((author) => {
+            const data = {
+                author,
+            }
+
+            res.status(200);
+            res.send(data);
+        })
+        .catch((error) => {
+            res.status(500);
+            res.send(error);
+        })
+}
+
+module.exports = {
+    login,
+    register,
+}
---
+++ app/controllers/blog_controllers/BlogControllers.js
@@ -0,0 +1,105 @@
+const Blog = require("../../models/blog");
+
+const all = (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+
+    Blog.find()
+        .then((blogs) => {
+            res.status(200);
+            res.send(blogs);
+        })
+        .catch((error) => {
+            res.status(500);
+            res.send(error);
+        });
+}
+
+const add = (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+
+    const blogData = req.body;
+
+    const blog = new Blog(blogData);
+    blog.save()
+        .then((blogResult) => {
+            const data = {
+                id: blogResult._id,
+            }
+
+            res.status(200);
+            res.send(data);
+        })
+        .catch((error) => {
+            res.status(500);
+            res.send(error);
+        });
+}
+
+const get = (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+
+    const blog_id = req.params.blog_id;
+
+    Blog.findByIdAndUpdate(blog_id, { $inc: { "views" : 1 }})
+        .then((blog) => {
+            const data = {
+                blog,
+            }
+
+            res.status(200);
+            res.send(data);
+        })
+        .catch((error) => {
+            res.status(500);
+            res.send(error);
+        });
+}
+
+const del = (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+
+    const blog_id = req.params.blog_id;
+
+    Blog.findByIdAndDelete(blog_id)
+        .then((blog) => {
+            const data = {
+                message: "Deleted",
+            }
+
+            res.status(200);
+            res.send(data);
+        })
+        .catch((error) => {
+            res.status(500);
+            res.send(error);
+        });
+}
+
+const update = (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+
+    const blog_id = req.body.id;
+    const newBlog = req.body.new;
+
+    Blog.findByIdAndUpdate(blog_id, newBlog)
+        .then((blog) => {
+            const data = {
+                message: "Updated",
+            }
+
+            res.status(200);
+            res.send(data);
+        })
+        .catch((error) => {
+            res.status(500);
+            res.send(error);
+        });
+}
+
+module.exports = {
+    all,
+    add,
+    get,
+    del,
+    update,
+}
