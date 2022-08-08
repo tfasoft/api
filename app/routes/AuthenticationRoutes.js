50a2d749041a4956c7f67345b8d560712d2fa1c1
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-08 13:02:42 +0430

    Refactor API to Authentication

--- app/routes/AuthenticationRoutes.js
+++ app/routes/AuthenticationRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const controllers = require('../controllers/APIControllers');
+const controllers = require('../controllers/AuthenticationControllers');
 
 const router = express.Router();
 
