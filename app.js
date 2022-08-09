d85c30039f7ea1fef50ba77f59fe9c428eb2cfb6
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-09 22:05:04 +0430

    Comments in runner

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-const mongoose = require('mongoose');
-const app = require('./app/app');
+// Requires
+const mongoose = require('mongoose'); // Require MongoDB drive
+const app = require('./app/app'); // Import app
 
+// Init ENV
 require('dotenv').config();
 const env = process.env;
 
+// Add MongoDB URL
 const mdb = `mongodb+srv://${env.MONGO_USERNAME}:${env.MONGO_PASSWORD}@${env.MONGO_DATABASE}.ji4jf.mongodb.net/${env.MONGO_COLLECTION}?retryWrites=true&w=majority`;
+
+// Connect MongoDB
 mongoose.connect(mdb)
     .then((connection) => {
-        const port = env.PORT;
+        const port = env.PORT; // Use port in .env
 
-        console.log(`Connected. Running in ${port}`)
-        app.listen(port);
+        console.log(`Connected. Running in ${port}`); // Print connected
+        app.listen(port); // Start app
     })
     .catch((error) => console.log(error));
