d5faf14ab172d5cdd2f4d3b1d2317ea58481ac78
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-09 18:07:34 +0430

    Add regenerate mcode route

--- app/routes/user_routes/UserRoutes.js
+++ app/routes/user_routes/UserRoutes.js
@@ -7,6 +7,7 @@ router.post('/login', controllers.login);
 router.post('/register', controllers.regsiter);
 router.post('/info', controllers.info);
 router.post('/get', controllers.getUser);
-router.post('/connect', controllers.connect)
+router.post('/connect', controllers.connect);
+router.post('/mcode/regenerate', controllers.mcode_regenerate);
 
 module.exports = router;
