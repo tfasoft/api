4a2b387ca98f3d3b2113e8922e2282c14e2dd3b3
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-04-09 22:00:35 +0330

    Runner

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 // Requires
 
-import { appConfig } from "@/config";
-import { mongodb } from "@/connections";
+import { appConfig } from "$app/config/index.js";
+import { user, admin } from "$app/connections/index.js";
 
 import app from "./app/index.js";
 
-mongodb.on("open", () => {
-  app.listen(appConfig.port, async () => {
-    console.log(`App is running on ${appConfig.port}`);
-  });
+user.on("error", () => {
+  console.log("Error in connecting user");
+  process.exit(1);
+});
+
+admin.on("error", () => {
+  console.log("Error in connecting admin");
+  process.exit(1);
+});
+
+app.listen(appConfig.port, () => {
+  console.log(`App is running on ${appConfig.port}`);
 });
