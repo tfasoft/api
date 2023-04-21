a62d15b9b75e90f8512fb0c932d100e43d500885
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-04-22 00:35:15 +0330

    Add logs

--- app/routes/user/user.routes.js
+++ app/routes/user/user.routes.js
@@ -10,5 +10,6 @@ import { User } from "$app/controllers/index.js";
 const router = express.Router();
 
 router.get("/:tid", User.SINGLE);
+router.get("/logs/:id", User.MY_LOGS);
 
 export default router;
