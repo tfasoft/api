c7919c4b5fc526ad1c348be244d274add95f2aa2
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-04-22 15:20:48 +0330

    Fix message again

--- app/controllers/access/access.controllers.js
+++ app/controllers/access/access.controllers.js
@@ -42,7 +42,6 @@ export const ACCESS = async (req, res) => {
                 "New login detected! 🔍\n",
                 `Application: ${serviceResult.name}`,
                 `Provider: ${adminResult.companyName}`,
-                "\n",
                 "If you was not that, check your active sessions that is there any other devices loged in to your account or not.\n",
                 "TFAsoft LLC",
               ];
