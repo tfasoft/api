e7f1af84e229801610b1e9c256122855d4f1bda2
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-05-01 22:45:35 +0330

    Work on redis connected

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 // Requires
 
 import { appConfig } from "$app/config/index.js";
+import { redis } from "$app/connections/index.js";
 
 import app from "./app/index.js";
 
-app.listen(appConfig.port, () => {
-  console.log(`App is running on ${appConfig.port}`);
-});
+redis.on("connect", () =>
+  app.listen(appConfig.port, () => {
+    console.log(`App is running on ${appConfig.port}`);
+  })
+);
