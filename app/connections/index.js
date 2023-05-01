5af80086b820f55a60fce7d61149b5de78e1bd4a
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-05-01 22:45:25 +0330

    Add redis

--- app/connections/index.js
+++ app/connections/index.js
@@ -1,5 +1,6 @@
+import redis from "$app/connections/redis/redis.connection.js";
 import admin from "$app/connections/admin/admin.connection.js";
 import user from "$app/connections/user/user.connection.js";
 import log from "$app/connections/log/log.connection.js";
 
-export { admin, user, log };
+export { redis, admin, user, log };
